6d47bf56ea09b517597ce510ed5c3170114fad96
Sukrut K Pasumarthi <kesarisukrut@gmail.com>
2024-10-14 21:15:12 -0400

    Add in-memory user details service and security configuration
    - Configure `SecurityFilterChain` to enforce authentication for specific endpoints and enable form-based login and HTTP Basic authentication.
    - Set up `UserDetailsService` with in-memory users: "user" with password "password" and "admin" with password "12345".

--- eazy-backend-app/src/main/java/com/spring/security/eazybackendapp/config/SecurityConfiguration.java
+++ eazy-backend-app/src/main/java/com/spring/security/eazybackendapp/config/SecurityConfiguration.java
@@ -3,6 +3,10 @@ package com.spring.security.eazybackendapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -28,4 +32,23 @@ public class SecurityConfiguration {
         http.httpBasic(withDefaults());
         return http.build();
     }
+
+    /**
+     * Configures the user details service.
+     * This method sets up the user details service with two users: one with the username
+     * "user" and the password "password",
+     * and another with the username "admin" and the password "12345".
+     *
+     * @return the configured UserDetailsService
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}password").roles("USER").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}12345").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+
+
+
 }
