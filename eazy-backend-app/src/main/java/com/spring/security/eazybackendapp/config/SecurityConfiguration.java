7f54d5a474f15885eb49bcc9158d4869df82f0c1
Sukrut K Pasumarthi <kesarisukrut@gmail.com>
2024-10-14 21:25:24 -0400

    dd security configuration and controllers for account, cards, and loans
    - Implement `AccountController` with `/account-details` endpoint.
    - Implement `CardsController` with `/cards` endpoint.
    - Implement `LoansController` with `/loans` endpoint.
    - Configure security settings in `SecurityConfiguration` to enforce authentication for specific endpoints.
    - Set up in-memory user details service with BCrypt password encoding.
    - Add compromised password checker using HaveIBeenPwned API.

--- eazy-backend-app/src/main/java/com/spring/security/eazybackendapp/config/SecurityConfiguration.java
+++ eazy-backend-app/src/main/java/com/spring/security/eazybackendapp/config/SecurityConfiguration.java
@@ -2,12 +2,17 @@ package com.spring.security.eazybackendapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -43,12 +48,34 @@ public class SecurityConfiguration {
      */
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}password").roles("USER").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}12345").roles("ADMIN").build();
+        UserDetails user = User.withUsername("user").password("{noop}EazyBytesUser@123").roles("USER").build();
+
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$BoZJnTHruRNJ5P4LfdFeo.NvwIhcBuFRLpEcTHtUVDyv1UVVfcaC2").roles("ADMIN").build();
+
         return new InMemoryUserDetailsManager(user, admin);
     }
 
 
+    /**
+     * Configures the password encoder.
+     * This method sets up the password encoder with a BCrypt password encoder.
+     *
+     * @return the configured PasswordEncoder
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
+    /**
+     * Configures the compromised password checker.
+     * This method sets up the compromised password checker with the HaveIBeenPwnedRestApiPasswordChecker.
+     *
+     * @return the configured CompromisedPasswordChecker
+     */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
 
 }
